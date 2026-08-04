13d1f92f2b9a25135fb5c2a89ce27651562afdc9
mariomtzf-lab <mario_mtzf@hotmail.com>
2026-08-04 18:04:45 +0000

    CREANDO BASE DE DATOS

--- dbconfiguration/save_data.sql
+++ dbconfiguration/save_data.sql
@@ -21,3 +21,31 @@ VALUES
 ('Daniela Flores', 'daniela.flores18@example.com', '1234567807', '1996-03-01'),
 ('Héctor Serrano', 'hector.serrano19@example.com', '1234567808', '1982-02-11'),
 ('Patricia Vega', 'patricia.vega20@example.com', '1234567809', '1990-09-05');
+
+
+
+
+
+-- Insertar datos en la tabla credenciales
+INSERT INTO credenciales (id_usuario, username, password_hash)
+VALUES
+(1, 'juan.perez1', 'hash_juan_perez'),
+(2, 'ana.gomez2', 'hash_ana_gomez'),
+(3, 'luis.martinez3', 'hash_luis_martinez'),
+(4, 'maria.lopez4', 'hash_maria_lopez'),
+(5, 'carlos.ruiz5', 'hash_carlos_ruiz'),
+(6, 'sofia.castro6', 'hash_sofia_castro'),
+(7, 'david.ramirez7', 'hash_david_ramirez'),
+(8, 'elena.ortega8', 'hash_elena_ortega'),
+(9, 'miguel.torres9', 'hash_miguel_torres'),
+(10, 'laura.sanchez10', 'hash_laura_sanchez'),
+(11, 'pedro.morales11', 'hash_pedro_morales'),
+(12, 'clara.hernandez12', 'hash_clara_hernandez'),
+(13, 'jorge.rojas13', 'hash_jorge_rojas'),
+(14, 'valeria.pena14', 'hash_valeria_pena'),
+(15, 'andres.romero15', 'hash_andres_romero'),
+(16, 'camila.paredes16', 'hash_camila_paredes'),
+(17, 'ricardo.vargas17', 'hash_ricardo_vargas'),
+(18, 'daniela.flores18', 'hash_daniela_flores'),
+(19, 'hector.serrano19', 'hash_hector_serrano'),
+(20, 'patricia.vega20', 'hash_patricia_vega');
